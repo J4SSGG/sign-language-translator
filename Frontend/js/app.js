3ac059406fa46adcb37a4574caf0d04b5139403e
jmansilla <jcmansillam@correo.url.edu.gt>
2020-11-04 15:49:07 -0600

    Consumo de servicio de ejemplo
    Creación de ejemplo de consumo de datos en un link de prueba

--- Frontend/js/app.js
+++ Frontend/js/app.js
@@ -56,9 +56,28 @@ $('#buscar').click(function(){
       var request = new XMLHttpRequest();
       request.open('GET', 'https://ghibliapi.herokuapp.com/films', true);
       request.onload = function () {
-        // Begin accessing JSON data here
+        // Por el momento tenemos un ejemplo, al estar el servicio lo vamos a acoplar a lo que tengamos
         var data = JSON.parse(this.response);
         if (request.status >= 200 && request.status < 400) {
+          data.forEach(movie => {
+            /*const card = document.createElement('div');
+            card.setAttribute('class', 'card');
+      
+            const h1 = document.createElement('h1');
+            h1.textContent = movie.title;
+      
+            const p = document.createElement('p');
+            movie.description = movie.description.substring(0, 300);
+            p.textContent = `${movie.description}...`;
+      
+            container.appendChild(card);
+            card.appendChild(h1);
+            card.appendChild(p);*/
+            console.log(movie.description);
+          });  
+
+
+
           console.log(data);
         } else {
           const errorMessage = document.createElement('marquee');
