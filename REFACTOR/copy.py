dd163d21855c90b7795be382634f61ba2b1bfa63
JOSEPH ABRAHAM SOTO GUTIERREZ <jasotog@correo.url.edu.gt>
2020-11-17 15:51:05 -0600

    Modelo resumido

---
+++ REFACTOR/copy.py
@@ -0,0 +1,6 @@
+import shutil
+import os
+count = 2
+while (count < 2400):
+    shutil.copy2('C:\\Users\\jasgs\\Documents\\tec-emergentes-proyecto\\sing-language-translator\\REFACTOR\\gestures\\_\\1.jpg', 'C:\\Users\\jasgs\\Documents\\tec-emergentes-proyecto\\sing-language-translator\\REFACTOR\\gestures\\_\\{}.jpg'.format(count))
+    count = count + 1
