07c6294687501d1a5a33af5ecda0f200678c0d7d
JOSEPH ABRAHAM SOTO GUTIERREZ <jasotog@correo.url.edu.gt>
2020-11-17 16:27:51 -0600

    Cambios en modelo

--- app.py
+++ app.py
@@ -11,7 +11,7 @@ from sklearn.preprocessing import normalize
 
 
 # mapa de clases
-class_names = ['A', 'B', 'C', 'D', 'E', 'F', 'G', 'H', 'I', 'J', 'K', 'L', 'M', 'N', 'O', 'P', 'Q', 'R', 'S', 'T', 'U', 'V', 'W', 'X', 'Y', 'Z', '0','1', '2', '3', '4', '5', '6', '7', '8', '9', '10']
+class_names = ['A', 'C', 'E', 'I', 'L', 'N', 'O', 'R', 'S', 'T', 'U', '1', '2', '3', '4', '5', '6', '7', '8', '9', '10', ' ']
 # variable de configuracion
 img_height = 50
 img_width = 50
